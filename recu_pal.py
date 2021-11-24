3d50607fa1af915e9eaaeebd684c9c2705c1c544
Rafi R <50540190+rafirahim@users.noreply.github.com>
2021-11-24 13:48:48 +0530

    Update recu_pal.py

--- recu_pal.py
+++ recu_pal.py
@@ -1,5 +1,4 @@
-#Recirsive Method to find the length of the longest palindromic sub-string
-
+#Recirsive Method to find the length of the longest palindromic Subsequence 
 # Function to calculate max of two integers
 def max(x, y):
     if (x > y):
@@ -8,9 +7,11 @@ def max(x, y):
         return y
 
 
-# Function to find the length of the  largest possible sub-tring
-def len_long_sub_string(strn, i, j):
+# Function to find the length of the  largest possible Subsequence 
+def len_long_subsequence (strn, i, j):
     
+    if (i>j):
+        return 0
     # If there is only 1 character
     if (i==j):
         return 1
@@ -22,18 +23,18 @@ def len_long_sub_string(strn, i, j):
  
 
     # If the first and last characters are same
-    # Remove those charcters and new string go through function again
+    # Remove those charcters and new Subsequence go through function again
     # 2 is aaded to the lenth each time
     if (strn[i]==strn[j]):
-      return len_long_sub_string(strn, i+1, j-1) + 2
+      return len_long_subsequence(strn, i+1, j-1) + 2
 
     # If the first and last characters do not match
     #new strings taken one withot first charector and other without last charector
     # Both will go through the function and maximum will be returned
-    return max(len_long_sub_string(strn,i,j-1),len_long_sub_string(strn,i+1,j))
+    return max(len_long_subsequence(strn,i,j-1),len_long_subsequence(strn,i+1,j))
 
 
 string = input("Enter the string: ")
 n = len(string)
-print("The length of Largest pallindrome subsequence is ",len_long_sub_string(string,0,n-1))
+print("The length of Largest pallindrome subsequence is ",len_long_subsequence(string,0,n-1))
 
